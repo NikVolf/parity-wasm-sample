311805fe276e166ccfa14e4342b88392fb58b7b9
NikVolf <nikvolf@gmail.com>
2017-12-21 19:45:10 +0300

    tabs

--- src/lib.rs
+++ src/lib.rs
@@ -28,21 +28,21 @@ impl<'a, 'b> Runtime<'a, 'b> {
 			"env".into(),
 			Arc::new(
 				interpreter::env_native_module(
-                    env_instance,
-                    interpreter::UserDefinedElements {
+					env_instance,
+					interpreter::UserDefinedElements {
 
-                        /*
-                            Runtime implements UserFunctionExecutor, so is used `self` here
-                        */
-                        executor: Some(self),
-                        globals: ::std::collections::HashMap::new(),
+						/*
+							Runtime implements UserFunctionExecutor, so is used `self` here
+						*/
+						executor: Some(self),
+						globals: ::std::collections::HashMap::new(),
 
-                        /*
-                            This is your external functions signatures
-                        */
-                        functions: ::std::borrow::Cow::from(&[][..]),
-                    },
-                ).expect("Env module always exists; qed")
+						/*
+							This is your external functions signatures
+						*/
+						functions: ::std::borrow::Cow::from(&[][..]),
+					},
+				).expect("Env module always exists; qed")
 			)
 		)
 	}
@@ -54,9 +54,9 @@ impl<'a, 'b> interpreter::UserFunctionExecutor<interpreter::DummyUserError> for 
 	{
 		match name {
 
-            /*
-                This is where you handle all externals
-            */
+			/*
+				This is where you handle all externals
+			*/
 
 			"_your_cool_external" => {
 				panic!();
@@ -72,39 +72,39 @@ impl<'a, 'b> interpreter::UserFunctionExecutor<interpreter::DummyUserError> for 
 #[cfg(test)]
 mod tests {
 
-    use super::*;
+	use super::*;
 
-    use parity_wasm::ModuleInstanceInterface;
+	use parity_wasm::ModuleInstanceInterface;
 
-    fn run(ext: &mut Ext) {
-        let program = interpreter::ProgramInstance::new().expect("program instance to create");
-        let code_buffer = vec![];
+	fn run(ext: &mut Ext) {
+		let program = interpreter::ProgramInstance::new().expect("program instance to create");
+		let code_buffer = vec![];
 
-        // env module extracted from wasm program (it has one by default)
+		// env module extracted from wasm program (it has one by default)
 		let env_instance = program.module("env")
-            .expect("Wasm program to contain env module");
+			.expect("Wasm program to contain env module");
 
-        // linear memory extracted from env module (it has one by default)
+		// linear memory extracted from env module (it has one by default)
 		let env_memory = env_instance.memory(interpreter::ItemIndex::Internal(0))
-            .expect("Linear memory to exist in wasm runtime");
+			.expect("Linear memory to exist in wasm runtime");
 
-        // creating runtime
+		// creating runtime
 		let mut runtime = Runtime {
-            ext: ext,
-            program: &program,
-            memory: env_memory.clone(),
-        };
+			ext: ext,
+			program: &program,
+			memory: env_memory.clone(),
+		};
 
 		{
-            let module = parity_wasm::deserialize_buffer(code_buffer).expect("a valid module");
+			let module = parity_wasm::deserialize_buffer(code_buffer).expect("a valid module");
 
 			let execution_params = runtime.execution_params()
 				.add_argument(interpreter::RuntimeValue::I32(0));
 
 			let module_instance = program.add_module("module", module, Some(&execution_params.externals))
-                .expect("should be added without errors");
+				.expect("should be added without errors");
 
 			module_instance.execute_export("_your_cool_entry_point", execution_params).expect("should run without errors");
 		}
-    }
+	}
 }
